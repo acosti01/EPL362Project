a1b90f1c97fc02c86b7bede4c7fe953dff82993b
acosti01 <acosti01@cs.ucy.ac.cy>
2017-05-05 22:32:29 +0300

    a

--- src/rmiinterface/RMIInterface.java
+++ src/rmiinterface/RMIInterface.java
@@ -6,16 +6,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface RMIInterface extends Remote {
-	
-	public ArrayList<String> validate(String username, String password) throws RemoteException, SQLException;
 
-	public Object[][] fillDiary() throws RemoteException,SQLException;
+	public ArrayList<String> validate(String username, String password)
+			throws RemoteException, SQLException;
+
+	public Object[][] fillDiary() throws RemoteException, SQLException;
 
 	Object[][] fillPatients() throws RemoteException, SQLException;
 
 	void addPatient(int id, String name, String surname, String email,
 			String address, String bday, String tel, String gender)
 			throws RemoteException, SQLException;
-	
-		
+
 }
