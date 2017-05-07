d2ca8f32c4f826ce790c468c7a4199c1259ad790
acosti01 <acosti01@cs.ucy.ac.cy>
2017-05-07 11:29:38 +0300

    server

--- src/rmiserver/ServerOperation.java
+++ src/rmiserver/ServerOperation.java
@@ -9,9 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
-import javax.swing.JTextField;
-
 import rmiinterface.RMIInterface;
 
 public class ServerOperation extends UnicastRemoteObject implements RMIInterface {
