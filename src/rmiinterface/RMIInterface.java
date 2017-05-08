3bd9c99569f6c07f38e6d09fef231b00e4440502
acosti01 <acosti01@cs.ucy.ac.cy>
2017-05-08 15:02:17 +0300

    interface

--- src/rmiinterface/RMIInterface.java
+++ src/rmiinterface/RMIInterface.java
@@ -5,8 +5,6 @@ import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.swing.JTextField;
-
 public interface RMIInterface extends Remote {
 
 	public ArrayList<String> validate(String username, String password) throws RemoteException, SQLException;
